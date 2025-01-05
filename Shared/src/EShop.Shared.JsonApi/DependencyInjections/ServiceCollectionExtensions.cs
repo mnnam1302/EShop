89031e104363b52fac42e994a834bfa56965ca3e
Nhat Nam <mnnam.khtn@gmail.com>
2025-01-05 16:07:17 +0700

    Apply rule "no-side effect" in DbInitializer

--- Shared/src/EShop.Shared.JsonApi/DependencyInjections/ServiceCollectionExtensions.cs
+++ Shared/src/EShop.Shared.JsonApi/DependencyInjections/ServiceCollectionExtensions.cs
@@ -21,6 +21,7 @@ public static class ServiceCollectionExtensions
         services.AddHttpContextAccessor();
         services.AddScoped<IUserDetailsProvider, HttpRequestUserDataProvider>();
         services.AddTransient<IMultiTenantIsolationStategy, PostgresMultiTenantConnectionInterceptor>();
+        services.AddTransient<MultiTenantSaveChangesInterceptor>();
         services.AddScoped<ITenantIsolationStrategy, PostgresRowLevelSecurityPolicyIsolation>();
         
         return services;
