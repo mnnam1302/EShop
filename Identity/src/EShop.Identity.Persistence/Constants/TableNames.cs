b7a044a0369a7b2ad731b1222aea8e42e4c7b925
Nhat Nam <mnnam.khtn@gmail.com>
2025-01-05 15:45:08 +0700

    Used nameof operator to avoid hard code in table name constants

--- Identity/src/EShop.Identity.Persistence/Constants/TableNames.cs
+++ Identity/src/EShop.Identity.Persistence/Constants/TableNames.cs
@@ -2,13 +2,13 @@
 
 public static class TableNames
 {
-    public const string Organizations = "Organizations";
-    public const string Users = "Users";
-    public const string Roles = "Roles";
-    public const string Permissions = "Permissions";
-    public const string RolePermissions = "RolePermissions";
-    public const string UserRoles = "UserRoles";
+    public const string Organizations = nameof(Organizations);
+    public const string Users = nameof(Users);
+    public const string Roles = nameof(Roles);
+    public const string Permissions = nameof(Permissions);
+    public const string RolePermissions = nameof(RolePermissions);
+    public const string UserRoles = nameof(UserRoles);
 
-    public const string Tenants = "Tenants";
-    public const string TenantSettings = "TenantSettings";
+    public const string Tenants = nameof(Tenants);
+    public const string TenantSettings = nameof(TenantSettings);
 }
