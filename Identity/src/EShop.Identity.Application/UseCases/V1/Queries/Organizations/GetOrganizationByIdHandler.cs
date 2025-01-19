7310e65dbebc65e5d7fecb194d664c55384c8e68
Kodi Mai <kodi.mai@seamless.insure>
2025-01-20 00:52:27 +0700

    Implemented get organization by id

---
+++ Identity/src/EShop.Identity.Application/UseCases/V1/Queries/Organizations/GetOrganizationByIdHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Eshop.Shared.DomainTools.DomainExceptions;
+using EShop.Identity.Domain.Abstractions.Repositories;
+using EShop.Identity.Domain.Entities;
+using EShop.Shared.Contracts.Abstractions.Requests;
+using EShop.Shared.Contracts.Abstractions.Shared;
+using EShop.Shared.Contracts.Services.Identity.Organizations;
+
+namespace EShop.Identity.Application.UseCases.V1.Queries.Organizations;
+
+public class GetOrganizationByIdHandler : IQueryHandler<Query.GetOrganizationById, Response.OrganizationResponse>
+{
+    private readonly IIdentityAggregateRepository<Organization, string> _organizationRepository;
+    private readonly IMapper _mapper;
+
+    public GetOrganizationByIdHandler(
+        IIdentityAggregateRepository<Organization, string> organizationRepository,
+        IMapper mapper)
+    {
+        _organizationRepository = organizationRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<Response.OrganizationResponse>> Handle(Query.GetOrganizationById request, CancellationToken cancellationToken)
+    {
+        var organization = await _organizationRepository.FindByIdAsync(request.Id);
+        if (organization == null)
+        {
+            throw new NotFoundException("Organization is not found");
+        }
+
+        var result = _mapper.Map<Response.OrganizationResponse>(organization);
+        return Result.Success(result);
+    }
+}
---
+++ Shared/src/EShop.Shared.Contracts/Services/Identity/Organizations/Response.cs
@@ -0,0 +1,18 @@
+﻿namespace EShop.Shared.Contracts.Services.Identity.Organizations;
+
+public static class Response
+{
+    public record OrganizationResponse
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string? OrganizationNumber { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Postcode { get; set; }
+        public string? Description { get; set; }
+        public string? ParentOrganizationId { get; set; }
+    }
+}
