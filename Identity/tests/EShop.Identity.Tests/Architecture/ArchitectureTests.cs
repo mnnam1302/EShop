03a23a30312ef175301f5975c1ee6c4ffe2cfc2b
Nhat Nam <mnnam.khtn@gmail.com>
2025-01-29 22:52:20 +0700

    Implemented an architecture test in clean architecture

--- Identity/tests/EShop.Identity.Tests/Architecture/ArchitectureTests.cs
+++ Identity/tests/EShop.Identity.Tests/Architecture/ArchitectureTests.cs
@@ -7,8 +7,8 @@ public class ArchitectureTests
 {
     private const string DomainNamespace = "EShop.Identity.Domain";
     private const string ApplicationNamespace = "EShop.Identity.Application";
-    private const string InfrastructureNamespace = "EShop.Identity.Infrastructure";
     private const string PersistenceNamespace = "EShop.Identity.Persistence";
+    private const string InfrastructureNamespace = "EShop.Identity.Infrastructure";
     private const string PresentationNamespace = "EShop.Identity.Presentation";
     private const string ApiNamespace = "EShop.Identity.Api";
 
@@ -27,6 +27,101 @@ public class ArchitectureTests
 
         var result = Types
             .InAssembly(domain)
+            .ShouldNot()
+            .HaveDependencyOnAny(otherProjects)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_Not_HaveDependencyOtherProjects()
+    {
+        var application = Application.AssemblyReference.Assembly;
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            PersistenceNamespace,
+            PresentationNamespace,
+            ApiNamespace
+        };
+
+        var result = Types
+            .InAssembly(application)
+            .ShouldNot()
+            .HaveDependencyOnAny(otherProjects)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    //[Fact]
+    //public void Handlers_Should_HaveDependencyOnDomain()
+    //{
+    //    var application = Application.AssemblyReference.Assembly;
+
+    //    var result = Types
+    //        .InAssembly(application)
+    //        .That()
+    //        .HaveNameEndingWith("Handler")
+    //        .Should()
+    //        .HaveDependencyOn(DomainNamespace)
+    //        .GetResult();
+
+    //    result.IsSuccessful.Should().BeTrue();
+    //}
+
+    [Fact]
+    public void Persistence_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        var persistence = Persistence.AssemblyReference.Assembly;
+        var otherProjects = new[]
+        {
+            PresentationNamespace,
+            ApiNamespace
+        };
+
+        var result = Types
+            .InAssembly(persistence)
+            .ShouldNot()
+            .HaveDependencyOnAny(otherProjects)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Infrastructure_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        var infrastructure = Infrastructure.AssemblyReference.Assembly;
+        var otherProjects = new[]
+        {
+            PresentationNamespace,
+            ApiNamespace
+        };
+
+        var result = Types
+            .InAssembly(infrastructure)
+            .ShouldNot()
+            .HaveDependencyOnAny(otherProjects)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Presentation_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        var presentation = Presentation.AssemblyReference.Assembly;
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            PersistenceNamespace,
+            DomainNamespace,
+        };
+
+        var result = Types
+            .InAssembly(presentation)
             .ShouldNot()
             .HaveDependencyOnAny(otherProjects)
             .GetResult();
