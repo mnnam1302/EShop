43bc09cc36607e1edc4120a456b074c21f872fc6
Kodi Mai <kodi.mai@seamless.insure>
2025-02-10 00:17:26 +0700

    Created the Tenant and Feature aggregate in domain layer

---
+++ Tenancy/src/EShop.Tenancy.Domain/Enumerations/StateFeature.cs
@@ -0,0 +1,20 @@
+﻿using Ardalis.SmartEnum;
+
+namespace EShop.Tenancy.Domain.Enumerations;
+
+public class StateFeature : SmartEnum<StateFeature>
+{
+    public StateFeature(string name, int value)
+        : base(name, value)
+    {
+    }
+
+    public static readonly StateFeature Enabled = new StateFeature(nameof(Enabled), 1);
+    public static readonly StateFeature Disabled = new StateFeature(nameof(Disabled), 2);
+
+    public static StateFeature FromValue(int value) => FromValue(value);
+    public static StateFeature FromName(string name) => FromName(name);
+
+    public static implicit operator int(StateFeature state) => state.Value;
+    public static implicit operator string(StateFeature state) => state.Name;
+}
---
+++ Tenancy/src/EShop.Tenancy.Domain/ModelConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace EShop.Tenancy.Domain;
+
+public class ModelConstants
+{
+    public const int TinyText = 10;
+    public const int ShortText = 50;
+    public const int ShortMediumText = 100;
+    public const int MediumText = 150;
+    public const int MediumLongText = 200;
+    public const int StandardText = 255;
+    public const int LongText = 500;
+}
