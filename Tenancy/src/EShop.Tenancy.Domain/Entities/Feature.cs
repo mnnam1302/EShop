82eaf84568e0ad560e978c48d6d6ea3a833c948a
Kodi Mai <kodi.mai@seamless.insure>
2025-02-22 14:41:45 +0700

    Applied central package management technique for the solution.

---
+++ Tenancy/src/EShop.Tenancy.Domain/Entities/Feature.cs
@@ -0,0 +1,25 @@
+﻿using EShop.Tenancy.Domain.Enumerations;
+using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Tenancy.Domain.Entities;
+
+public class Feature
+{
+    [MaxLength(ModelConstants.ShortMediumText)]
+    public string Name { get; set; }
+
+    [MaxLength(ModelConstants.LongText)]
+    public string? Description { get; set; }
+
+    [MaxLength(ModelConstants.MediumText)]
+    public string Module { get; set; }
+
+    [MaxLength(ModelConstants.MediumText)]
+    public string State { get; set; } = StateFeature.Enabled;
+
+    [MaxLength(ModelConstants.MediumText)]
+    public string DefaultStateForNewTenant { get; set; } = StateFeature.Disabled;
+
+    [MaxLength(ModelConstants.MediumText)]
+    public string? Category { get; set; }
+}
