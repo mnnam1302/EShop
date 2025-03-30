8d9c6b3bb92f29ef2ea739ed7df45977ec911a33
Kodi Mai <kodi.mai@seamless.insure>
2025-03-30 13:16:56 +0700

    Add migration update InboxMessages table for Tenancy service.

---
+++ Tenancy/src/EShop.Tenancy.Persistence/Migrations/20250330061449_AddState_ReasonFailed_UpdatedOnUtc_InboxMessagesTable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Tenancy.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddState_ReasonFailed_UpdatedOnUtc_InboxMessagesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReasonFailed",
+                table: "InboxMessages",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "InboxMessages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOnUtc",
+                table: "InboxMessages",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReasonFailed",
+                table: "InboxMessages");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "InboxMessages");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOnUtc",
+                table: "InboxMessages");
+        }
+    }
+}
