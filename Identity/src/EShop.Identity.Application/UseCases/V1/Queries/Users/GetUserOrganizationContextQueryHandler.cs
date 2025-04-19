e6315db3372c0f9992061cfac97edf7ba8414a8b
Kodi Mai <kodi.mai@seamless.insure>
2025-04-19 08:29:12 +0700

    Implemented logic get user organization context query handler.

--- Identity/src/EShop.Identity.Application/UseCases/V1/Queries/Users/GetUserOrganizationContextQueryHandler.cs
+++ Identity/src/EShop.Identity.Application/UseCases/V1/Queries/Users/GetUserOrganizationContextQueryHandler.cs
@@ -1,5 +1,22 @@
-﻿namespace EShop.Identity.Application.UseCases.V1.Queries.Users;
+﻿using EShop.Shared.Contracts.Abstractions.Requests;
+using EShop.Shared.Contracts.Abstractions.Shared;
+using EShop.Shared.Contracts.Services.Identity.Users;
+using EShop.Shared.Scoping.ResourceAccessControl.Providers.UserOrganizationContextProvider;
 
-public sealed class GetUserOrganizationContextQueryHandler
+namespace EShop.Identity.Application.UseCases.V1.Queries.Users;
+
+public sealed class GetUserOrganizationContextQueryHandler : IQueryHandler<Query.GetUserOrganizationContextQuery, Response.UserOrganizationContext>
 {
+    private readonly IUserOrganizationContextProvider _userOrganizationContextProvider;
+
+    public GetUserOrganizationContextQueryHandler(IUserOrganizationContextProvider userOrganizationContextProvider)
+    {
+        _userOrganizationContextProvider = userOrganizationContextProvider;
+    }
+
+    public async Task<Result<Response.UserOrganizationContext>> Handle(Query.GetUserOrganizationContextQuery request, CancellationToken cancellationToken)
+    {
+        var userOrganizationContext = await _userOrganizationContextProvider.GetUserOrganizationContextAsync();
+        return userOrganizationContext;
+    }
 }
