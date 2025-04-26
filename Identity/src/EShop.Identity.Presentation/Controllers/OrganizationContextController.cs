abade1f03191bb9abd5ffc20be98bc4a312d5347
Nhat Nam <mnnam.khtn@gmail.com>
2025-04-26 15:24:36 +0700

    Introduced two new  APIs for get organization context by id and path.

---
+++ Identity/src/EShop.Identity.Presentation/Controllers/OrganizationContextController.cs
@@ -0,0 +1,47 @@
+﻿using Asp.Versioning;
+using EShop.Shared.Contracts.Services.Identity.Organizations;
+using EShop.Shared.JsonApi.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Identity.Presentation.Controllers;
+
+[ApiController]
+[Route("api/v{api:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+public class OrganizationContextController : ApiEndpointBase
+{
+    private readonly ISender _sender;
+
+    public OrganizationContextController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IResult> GetUserOrganizationContextById([FromRoute] string id)
+    {
+        var result = await _sender.Send(new Query.GetOrganizationContextByIdQuery(id));
+        
+        if (result.IsFailure)
+        {
+            return HandlerFailure(result);
+        }
+
+        return TypedResults.Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IResult> GetUserOrganizationContextByPath([FromQuery] string organizationContextPath)
+    {
+        var result = await _sender.Send(new Query.GetUserOrganizationContextByPathQuery(organizationContextPath));
+        
+        if (result.IsFailure)
+        {
+            return HandlerFailure(result);
+        }
+
+        return TypedResults.Ok(result);
+    }
+}
--- Shared/src/EShop.Shared.Contracts/Services/Identity/Organizations/Query.cs
+++ Shared/src/EShop.Shared.Contracts/Services/Identity/Organizations/Query.cs
@@ -4,5 +4,10 @@ namespace EShop.Shared.Contracts.Services.Identity.Organizations;
 
 public static class Query
 {
+    public record GetOrganizationContextByIdQuery(string OrganizationId) : IQuery<Response.OrganizationContext>;
+
+    public record GetUserOrganizationContextByPathQuery(string OrganizationContextPath) : IQuery<Response.OrganizationContext>;
+
     public record GetOrganizationById(string Id) : IQuery<Response.OrganizationResponse>;
+
 }
