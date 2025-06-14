96d3e35d8efaf51510f11dc310b252bc1377f3d5
Kodi Mai <kodi.mai@seamless.insure>
2025-06-14 15:04:04 +0700

    Add a new migration TenantSetting.

---
+++ Tenancy/src/EShop.Tenancy.Persistence/Migrations/20250614080212_AddTenantSettings.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Tenancy.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTenantSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerUsername",
+                table: "Tenants",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Tenants",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.CreateTable(
+                name: "TenantSettings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DisplayDateFormat = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    DisplayTimeFormat = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    TimeZone = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    DefaultCurrency = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    CurrencyDisplayFormat = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    DefaultSystemLanguage = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    TenantId = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Scope = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TenantSettings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TenantSettings_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TenantSettings_Scope",
+                table: "TenantSettings",
+                column: "Scope");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TenantSettings_TenantId",
+                table: "TenantSettings",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TenantSettings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerUsername",
+                table: "Tenants",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Tenants",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
