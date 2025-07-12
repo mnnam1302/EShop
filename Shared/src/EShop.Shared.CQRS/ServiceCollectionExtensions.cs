95a84cf01de6dec1a27ac6f671c04217b80bcf24
Kodi Mai <kodi.mai@seamless.insure>
2025-07-12 18:21:04 +0700

    Refactor code.

--- Shared/src/EShop.Shared.CQRS/ServiceCollectionExtensions.cs
+++ Shared/src/EShop.Shared.CQRS/ServiceCollectionExtensions.cs
@@ -8,121 +8,37 @@ namespace EShop.Shared.CQRS;
 
 public static class ServiceCollectionExtensions
 {
-    /// <summary>
-    /// Adds CQRS services to the dependency injection container
-    /// </summary>
-    /// <param name="services">The service collection</param>
-    /// <param name="assemblies">Assemblies to scan for handlers</param>
-    /// <returns>The service collection</returns>
     public static IServiceCollection AddCqrs(this IServiceCollection services, params Assembly[] assemblies)
     {
-        // Register core CQRS services
         services.AddScoped<ICommandDispatcher, CommandDispatcher>();
         services.AddScoped<IQueryDispatcher, QueryDispatcher>();
         services.AddScoped<IMediator, Mediator>();
 
-        // Register handlers from assemblies
         foreach (var assembly in assemblies)
         {
-            RegisterAllHandlers(services, assembly);
+            RegisterHandlers(services, assembly, typeof(ICommandHandler<>));
+            RegisterHandlers(services, assembly, typeof(ICommandHandler<,>));
+            RegisterHandlers(services, assembly, typeof(IQueryHandler<,>));
         }
 
         return services;
     }
 
-    /// <summary>
-    /// Adds CQRS services and scans the calling assembly for handlers
-    /// </summary>
-    /// <param name="services">The service collection</param>
-    /// <returns>The service collection</returns>
-    public static IServiceCollection AddCqrs(this IServiceCollection services)
+    private static void RegisterHandlers(IServiceCollection services, Assembly assembly, Type handlerInterfaceType)
     {
-        var callingAssembly = Assembly.GetCallingAssembly();
-        return services.AddCqrs(callingAssembly);
-    }
-
-    /// <summary>
-    /// Registers all command and query handlers from the specified assemblies
-    /// </summary>
-    /// <param name="services">The service collection</param>
-    /// <param name="assemblies">Assemblies to scan</param>
-    /// <returns>The service collection for chaining</returns>
-    public static IServiceCollection AddHandlersFromAssemblies(this IServiceCollection services, params Assembly[] assemblies)
-    {
-        foreach (var assembly in assemblies)
-        {
-            // Register command handlers (with result)
-            var commandHandlerTypes = assembly.GetTypes()
-                .Where(type => type.IsClass && !type.IsAbstract)
-                .Where(type => type.GetInterfaces()
-                    .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ICommandHandler<,>)))
-                .ToList();
-
-            foreach (var handlerType in commandHandlerTypes)
-            {
-                var interfaceType = handlerType.GetInterfaces()
-                    .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ICommandHandler<,>));
-
-                services.TryAddScoped(interfaceType, handlerType);
-            }
-        }
-
-        return services;
-    }
-
-    private static void RegisterAllHandlers(IServiceCollection services, Assembly assembly)
-    {
-        // Register command handlers (void)
-        var commandHandlerTypes = assembly.GetTypes()
+        var handlerTypes = assembly.GetTypes()
             .Where(t => t.IsClass && !t.IsAbstract)
             .Where(t => t.GetInterfaces()
-                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ICommandHandler<>)))
-            .ToList();
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerInterfaceType));
 
-        foreach (var handlerType in commandHandlerTypes)
+        foreach (var handlerType in handlerTypes)
         {
-            var handlerInterfaces = handlerType.GetInterfaces()
-                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ICommandHandler<>));
+            var interfaces = handlerType.GetInterfaces()
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerInterfaceType);
 
-            foreach (var handlerInterface in handlerInterfaces)
+            foreach (var @interface in interfaces)
             {
-                services.TryAddScoped(handlerInterface, handlerType);
-            }
-        }
-
-        // Register command handlers (with result)
-        var commandHandlerWithResultTypes = assembly.GetTypes()
-            .Where(t => t.IsClass && !t.IsAbstract)
-            .Where(t => t.GetInterfaces()
-                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ICommandHandler<,>)))
-            .ToList();
-
-        foreach (var handlerType in commandHandlerWithResultTypes)
-        {
-            var handlerInterfaces = handlerType.GetInterfaces()
-                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ICommandHandler<,>));
-
-            foreach (var handlerInterface in handlerInterfaces)
-            {
-                services.TryAddScoped(handlerInterface, handlerType);
-            }
-        }
-
-        // Register query handlers
-        var queryHandlerTypes = assembly.GetTypes()
-            .Where(t => t.IsClass && !t.IsAbstract)
-            .Where(t => t.GetInterfaces()
-                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IQueryHandler<,>)))
-            .ToList();
-
-        foreach (var handlerType in queryHandlerTypes)
-        {
-            var handlerInterfaces = handlerType.GetInterfaces()
-                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IQueryHandler<,>));
-
-            foreach (var handlerInterface in handlerInterfaces)
-            {
-                services.TryAddScoped(handlerInterface, handlerType);
+                services.TryAddScoped(@interface, handlerType);
             }
         }
     }
