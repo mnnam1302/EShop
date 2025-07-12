6badee547e566fc57524a475603092798ea986e5
Kodi Mai <kodi.mai@seamless.insure>
2025-07-12 15:19:45 +0700

    Add a new migration.

--- Configuration/src/EShop.Configuration.Application/Migrations/20250712081832_AddProduct.Designer.cs
+++ Configuration/src/EShop.Configuration.Application/Migrations/20250712081832_AddProduct.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EShop.Configuration.Application.Shared;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EShop.Configuration.Application.Migrations
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250712081832_AddProduct")]
+    partial class AddProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Configuration/src/EShop.Configuration.Application/Products/Product.cs
+++ Configuration/src/EShop.Configuration.Application/Products/Product.cs
@@ -1,12 +1,15 @@
 ﻿using EShop.Configuration.Application.Agencies;
 using EShop.Configuration.Application.SalesChannels;
+using EShop.Shared.Contracts.Shared;
 using EShop.Shared.DomainTools.Entities;
 using EShop.Shared.Scoping;
+using System.ComponentModel.DataAnnotations;
 
 namespace EShop.Configuration.Application.Products;
 
 public class Product : EntityBase<Guid>, IScoped
 {
+    [MaxLength(ModelConstants.MediumText)]
     public string Name { get; set; } = string.Empty;
 
     public Guid? AgencyId { get; set; }
@@ -23,14 +26,18 @@ public class Product : EntityBase<Guid>, IScoped
 
     public DateTimeOffset CreatedAtUtc { get; set; }
 
+    [MaxLength(ModelConstants.MediumText)]
     public string CreatedByUserId { get; set; } = string.Empty;
 
     public DateTimeOffset? LastModifiedAtUtc { get; set; }
 
+    [MaxLength(ModelConstants.MediumText)]
     public string? LastModifiedByUserId { get; set; } = string.Empty;
 
+    [MaxLength(ModelConstants.ShortText)]
     public string TenantId { get; private set; } = string.Empty;
 
+    [MaxLength(ModelConstants.VeryLongText)]
     public string Scope { get; private set; } = string.Empty;
 
     public virtual ICollection<SalesChannel> SalesChannels { get; set; } = [];
