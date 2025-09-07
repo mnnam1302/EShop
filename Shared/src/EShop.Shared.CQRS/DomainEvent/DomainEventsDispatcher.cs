d39fbc3b218c99bc7ff64f3e3a7d3efc54944fa4
Nhat Nam <mnnam.khtn@gmail.com>
2025-09-07 13:03:09 +0700

    Implement domain event dispatcher.

---
+++ Shared/src/EShop.Shared.CQRS/DomainEvent/DomainEventsDispatcher.cs
@@ -0,0 +1,40 @@
+﻿using EShop.Shared.Contracts.Abstractions.MessageBus;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace EShop.Shared.CQRS.DomainEvent;
+
+internal sealed class DomainEventsDispatcher : IDomainEventsDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public DomainEventsDispatcher(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default)
+    {
+        foreach (var domainEvent in domainEvents)
+        {
+            using IServiceScope scope = _serviceProvider.CreateScope();
+
+            Type handlerType = typeof(IDomainEventHandler<>).MakeGenericType(domainEvent.GetType());
+            IEnumerable<object?> handlers = scope.ServiceProvider.GetServices(handlerType);
+
+            foreach (object? handler in handlers)
+            {
+                if (handler is null)
+                {
+                    continue;
+                }
+
+                MethodInfo? methodInfo = handlerType.GetMethod("Handle");
+                if (methodInfo is not null)
+                {
+                    await (Task)methodInfo.Invoke(handler, [domainEvents, cancellationToken])!;
+                }
+            }
+        }
+    }
+}
---
+++ Shared/src/EShop.Shared.CQRS/DomainEvent/IDomainEventsDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using EShop.Shared.Contracts.Abstractions.MessageBus;
+
+namespace EShop.Shared.CQRS.DomainEvent;
+
+public interface IDomainEventsDispatcher
+{
+    Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+}
