8676e1dd8bf0c80ad8592326f26a784ac17e40b4
Nhat Nam <mnnam.khtn@gmail.com>
2025-10-05 23:57:47 +0700

    feat/auth: remove unused code.

--- Authorization/src/EShop.Authorization.API/APIs/AuthenticationEndpointHandler.cs
+++ Authorization/src/EShop.Authorization.API/APIs/AuthenticationEndpointHandler.cs
@@ -17,9 +17,6 @@ public static class AuthenticationEndpointHandler
             .HasApiVersion(1);
 
         group.MapPost("login", LoginAsync);
-        group.MapGet("public-key/{tenantId}", GetPublicKeyAsync);
-        group.MapGet("public-key/{tenantId}/{keyId}", GetSpecificPublicKeyAsync);
-        group.MapGet(".well-known/jwks/{tenantId}", GetJwksAsync);
 
         return endpoints;
     }
@@ -32,45 +29,6 @@ public static class AuthenticationEndpointHandler
         if (result.IsFailure)
         {
             return Results.Unauthorized();
-        }
-
-        return Results.Ok(result.Value);
-    }
-
-    private static async Task<IResult> GetPublicKeyAsync([FromRoute] string tenantId, [FromServices] IMediator mediator)
-    {
-        var query = new GetPublicKeyQuery(tenantId);
-        var result = await mediator.QueryAsync<GetPublicKeyQuery, PublicKeyResponse>(query);
-
-        if (result.IsFailure)
-        {
-            return Results.NotFound(result.Error);
-        }
-
-        return Results.Ok(result.Value);
-    }
-
-    private static async Task<IResult> GetSpecificPublicKeyAsync([FromRoute] string tenantId, [FromRoute] string keyId, [FromServices] IMediator mediator)
-    {
-        var query = new GetPublicKeyQuery(tenantId, keyId);
-        var result = await mediator.QueryAsync<GetPublicKeyQuery, PublicKeyResponse>(query);
-
-        if (result.IsFailure)
-        {
-            return Results.NotFound(result.Error);
-        }
-
-        return Results.Ok(result.Value);
-    }
-
-    private static async Task<IResult> GetJwksAsync([FromRoute] string tenantId, [FromServices] IMediator mediator)
-    {
-        var query = new GetJwksQuery(tenantId);
-        var result = await mediator.QueryAsync<GetJwksQuery, JwksResponse>(query);
-
-        if (result.IsFailure)
-        {
-            return Results.NotFound(result.Error);
         }
 
         return Results.Ok(result.Value);
