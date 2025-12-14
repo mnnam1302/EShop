0830d6d8e419ebf200e035ed4d1a623bea7db69e
Nhat Nam <mnnam.khtn@gmail.com>
2025-12-14 16:59:44 +0700

    Draft update product feature.

---
+++ Catalog/src/EShop.Catalog.Application/Products/Update/EndpointHandler.cs
@@ -0,0 +1,52 @@
+﻿using EShop.Shared.CQRS;
+using EShop.Shared.DomainTools.Exceptions;
+using EShop.Shared.JsonApi.Abstractions;
+using EShop.Shared.JsonApi.ResourceAccessControl;
+using EShop.Shared.Scoping.ResourceAccessControl;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Catalog.Application.Products.Update;
+
+public static class EndpointHandler
+{
+    public static RouteGroupBuilder MapUpdateProduct(this RouteGroupBuilder categoryEndpointBuilder)
+    {
+        categoryEndpointBuilder.MapPost("/{id}", UpdateProductAsync)
+            .RequirePermissionFilter(PermissionConstants.Catalog.ManageProducts);
+
+        return categoryEndpointBuilder;
+    }
+
+    private static async Task<IResult> UpdateProductAsync(
+        [FromRoute] string id,
+        [FromBody] UpdateProductRequest request,
+        [FromServices] IMediator mediator,
+        CancellationToken cancellationToken)
+    {
+        if (!Guid.TryParse(id, out var productId))
+        {
+            throw new BadRequestException($"Invalid product id {id}.");
+        }
+
+        var command = new UpdateProductCommand
+        {
+            Id = productId,
+            Name = request.Name,
+            Description = request.Description,
+            CategoryId = request.CategoryId,
+            Tags = request.Tags,
+            Slug = request.Slug,
+            Images = request.Images,
+            Groups = request.Groups
+        };
+
+        var result = await mediator.SendAsync(command, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return ApiEndpointHandler.Failure(result);
+        }
+
+        return Results.NoContent();
+    }
+}
---
+++ Catalog/src/EShop.Catalog.Application/Products/Update/UpdateProductCommand.cs
@@ -0,0 +1,24 @@
+﻿using EShop.Shared.Contracts.Abstractions.Shared;
+using EShop.Shared.CQRS.Command;
+
+namespace EShop.Catalog.Application.Products.Update;
+
+public sealed class UpdateProductCommand : ICommand
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Guid CategoryId { get; set; }
+    public string[] Tags { get; set; } = [];
+    public string Slug { get; set; } = string.Empty;
+    public string[] Images { get; set; } = [];
+    public Guid[] Groups { get; set; } = [];
+}
+
+public sealed class UpdateProductCommandHandler : ICommandHandler<UpdateProductCommand>
+{
+    public Task<Result> HandleAsync(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
