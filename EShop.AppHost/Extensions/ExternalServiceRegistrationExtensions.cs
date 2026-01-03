9ff42f777b438175303a102bb2c749225cda8c23
Nhat Nam <mnnam.khtn@gmail.com>
2026-01-04 00:30:41 +0700

    Add grafana resource into .NET Aspire, but no see data.

--- EShop.AppHost/Extensions/ExternalServiceRegistrationExtensions.cs
+++ EShop.AppHost/Extensions/ExternalServiceRegistrationExtensions.cs
@@ -22,6 +22,12 @@ public static class ExternalServiceRegistrationExtensions
             .WithArgs("--web.enable-otlp-receiver", "--config.file=/etc/prometheus/prometheus.yml")
             .WithHttpEndpoint(targetPort: 9090, name: "http");
 
+        var grafana = builder.AddContainer("grafana", "grafana/grafana")
+            .WithBindMount("../Deployment/config/grafana/config", "/etc/grafana", isReadOnly: true)
+            .WithBindMount("../Deployment/config/grafana/dashboards", "/var/lib/grafana/dashboards", isReadOnly: true)
+            .WithEnvironment("PROMETHEUS_ENDPOINT", prometheus.GetEndpoint("http"))
+            .WithHttpEndpoint(targetPort: 3000, name: "http");
+
         builder.AddOpenTelemetryCollector(ResourceNames.OpenTelemetryCollector, @"..\Deployment\config\otelcollector\config.yaml")
                .WithEnvironment("PROMETHEUS_ENDPOINT", $"{prometheus.GetEndpoint("http")}/api/v1/otlp"); ;
 
