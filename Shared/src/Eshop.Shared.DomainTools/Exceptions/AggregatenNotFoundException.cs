1d0955e803d7276d3c373cd8b8381e60249216ab
Nhat Nam <mnnam.khtn@gmail.com>
2026-02-24 21:03:21 +0700

    feat(docker): commit code before apply OpenSpec.

--- Shared/src/Eshop.Shared.DomainTools/Exceptions/AggregatenNotFoundException.cs
+++ Shared/src/Eshop.Shared.DomainTools/Exceptions/AggregatenNotFoundException.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace EShop.Shared.DomainTools.Exceptions;
 
 public sealed class AggregatenNotFoundException : NotFoundException
 {
-    public AggregatenNotFoundException(Guid aggregateId, MemberInfo aggregateType) 
+    public AggregatenNotFoundException(Guid aggregateId, MemberInfo aggregateType)
         : base($"{aggregateType.Name} with id {aggregateId} not found")
     {
     }
