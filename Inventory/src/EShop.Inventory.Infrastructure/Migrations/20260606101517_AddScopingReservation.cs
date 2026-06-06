6d03595ef4ce29c7fffa9d2589c14c4464789ddb
Nhat Nam <mnnam.khtn@gmail.com>
2026-06-06 17:15:51 +0700

    feat(inventory): add scoping for Reservation table

---
+++ Inventory/src/EShop.Inventory.Infrastructure/Migrations/20260606101517_AddScopingReservation.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.Inventory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddScopingReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Scope",
+                table: "Reservations",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                table: "Reservations",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Scope",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Reservations");
+        }
+    }
+}
