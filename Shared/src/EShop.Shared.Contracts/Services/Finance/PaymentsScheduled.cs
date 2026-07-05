82d59d6b1532c53fe7ceda32be888775c164d9cc
Nhat Nam <mnnam.khtn@gmail.com>
2026-07-05 23:15:27 +0700

    feat(finance): complete version 1 - booking payment to accounting provider

--- Shared/src/EShop.Shared.Contracts/Services/Finance/PaymentsScheduled.cs
+++
@@ -1,9 +0,0 @@
-using EShop.Shared.Contracts.Abstractions.MessageBus;
-
-namespace EShop.Shared.Contracts.Services.Finance;
-
-public sealed class PaymentsScheduled : IntegrationEvent
-{
-    public required Guid AccountId { get; init; }
-    public required Guid OrderId { get; init; }
-}
